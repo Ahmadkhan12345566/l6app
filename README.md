0c8be849d453c4a60ffd80c49624903740f1118b
Paper786 <arslanahmad12345566@gmail.com>
2020-09-10 01:10:09 +0500

    content updated http routing

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ laravel practice
 ###Content Completed 
 - laravel installation & project creation (laragon).
 - laravel file structure and booting system.
+- laravel routing methods hhtp get(with parameter& dynamic parameter),view,redirect,permanentRedirect and view.
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+// get,put,post,patch,delete,options
+//redirect,peramanentRedirect,view,any,match,middle,group
+Route::get('/home', function () { //clyser function
     return view('welcome');
 });
+//Required Route With Parameter
+//Route::get('welcome/{name}',function ($name){
+// return "Welcome ".$name;
+//});
+
+//Optional Route With Parameter
+//Route::get('welcome/{name?}',function ($name = "Guest"){
+//    return "Welcome ".$name;
+//});
+
+//Redirect route temp used in seo case not ranked
+//Route::redirect('/','home');
+
+//Redirect route temp
+//Route::permanentRedirect('/','home');
+
+//view
+
+Route::view('/','welcome',['name'=>"Arslan"]);
